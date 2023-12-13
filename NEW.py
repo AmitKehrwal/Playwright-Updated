25c7aec5c7477dce2b1f5ce54c0b056bdefeffdc
AmitKehrwal <144267580+AmitKehrwal@users.noreply.github.com>
2023-12-13 11:58:10 +0530

    Update NEW.py

--- NEW.py
+++ NEW.py
@@ -1,11 +1,11 @@
 import subprocess
+import os
 
 # Clone the repository
 subprocess.run(["git", "clone", "https://github.com/AmitKehrwal/Playwright-Updated.git"])
 
 # Change to the repository directory
 os.chdir("Playwright-Updated")
-
 
 # Give execution permission to install_brave.sh
 subprocess.run(["chmod", "+x", "install_brave.sh"])
