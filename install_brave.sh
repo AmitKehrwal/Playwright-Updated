8aedba6b388b2d3b4da754f26b4e501c742350ce
AmitKehrwal <144267580+AmitKehrwal@users.noreply.github.com>
2023-12-25 00:55:18 +0530

    Update install_brave.sh

--- install_brave.sh
+++ install_brave.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install -y curl
-
-# Download the Brave browser keyring
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-
-# Add Brave browser repository to sources.list.d
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
-# Update package list
-sudo apt-get update
-
-# Install Brave browser
-sudo apt-get install -y brave-browser
-
 # Install Python packages
 pip install selenium
 pip install indian_names
