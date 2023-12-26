4bd40b4b2e3021d302c8ef7d727ca3d46582ef5e
AmitKehrwal <144267580+AmitKehrwal@users.noreply.github.com>
2023-12-26 16:46:15 +0530

    Update NEW.py

--- NEW.py
+++ NEW.py
@@ -1,13 +1,1 @@
-import subprocess
-
-# Install Playwright and dependencies
-subprocess.run(['pip', 'install', 'playwright'])
-subprocess.run(['playwright', 'install'])
-subprocess.run(['pip', 'install', 'faker'])
-subprocess.run(['playwright', 'install-deps'])
-
-# Install additional system dependencies
-subprocess.run(['sudo', 'apt-get', 'install', '-y',
-                'libnss3', 'libnspr4', 'libatk-bridge2.0-0',
-                'libdrm2', 'libxkbcommon0', 'libatspi2.0-0',
-                'libgbm1', 'libasound2'])
+!pip install faker
