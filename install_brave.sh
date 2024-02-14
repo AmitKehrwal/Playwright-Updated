28a494dbdb133ecc38e4eb173be4648955b88284
AmitKehrwal <144267580+AmitKehrwal@users.noreply.github.com>
2024-02-14 22:19:01 +0530

    Update install_brave.sh

--- install_brave.sh
+++ install_brave.sh
@@ -3,13 +3,4 @@
 # Install Python packages
 pip install playwright
 playwright install
-pip install getindianname
-playwright install-deps
-sudo apt-get install -y libnss3 \
-                    libnspr4 \
-                    libatk-bridge2.0-0 \
-                    libdrm2 \
-                    libxkbcommon0 \
-                    libatspi2.0-0 \
-                    libgbm1 \
-                    libasound2
+pip install indian_names
