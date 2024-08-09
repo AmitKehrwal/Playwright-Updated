023975a946a3d69aaf39adce3de3179ff6c6fdc2
AmitKehrwal <144267580+AmitKehrwal@users.noreply.github.com>
2024-08-09 08:33:29 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,9 +1,1 @@
-!playwright install-deps
-!sudo apt-get install -y libnss3 \
-                     libnspr4 \
-                     libatk-bridge2.0-0 \
-                     libdrm2 \
-                     libxkbcommon0 \
-                     libatspi2.0-0 \
-                     libgbm1 \
-                     libasound2
+Nothing
